de6bf5a21645d53db951862bd3d93ae6eaee78ce
Tiancheng Jiang <48193263+Waynedoorer@users.noreply.github.com>
2025-09-25 15:03:48 +1000

    update the api for easily calculating separability scores for custom datasets

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,14 @@
 from . import components
 from . import models
 from . import tao_index
+from .api import tao_score
 
 """
 tao_index: data separability framework training pipeline
 components: core separability components & baselines
 models: classifiers and performance of public datasets (SECOM, SPF)
 utils: small helpers
+api: expose high level api usage to easily calculate separability score for custom datasets
 """
 
 __all__ = [
@@ -14,6 +16,7 @@ __all__ = [
     "models",
     "tao_index",
     "__version__",
+    "tao_score"
 ]
 
 __version__ = "0.1.0"
---
+++ src/api.py
@@ -0,0 +1,37 @@
+from src.components import calculate_ks_distance, compute_mmd, calculate_dunns_index, calculate_silhouette_score, \
+    calculate_davies_bouldin_index, calculate_N2, calculate_N4, calculate_T1, calculate_LSC
+import joblib
+
+def tao_score(X, y, model_path="Models/random_forest_model5.pkl", k=5):
+    """
+    Compute Tao Index score for a dataset.
+
+    Parameters
+    ----------
+    X : array-like (n_samples, n_features)
+        Feature matrix.
+    y : array-like (n_samples,)
+        Binary class labels (0/1).
+    model_path : str
+        Path to the trained Random Forest regression model.
+    k : int
+        Number of neighbors for local metrics.
+
+    Returns
+    -------
+    float
+        Predicted Tao Index score.
+    """
+    rf_model = joblib.load(model_path)
+
+    ks_distance, _ = calculate_ks_distance("distance", X, y, k=k)
+    ks_density, _ = calculate_ks_distance("density", X, y, k=k)
+    ks_dimension, _ = calculate_ks_distance("dimension", X, y, k=k)
+    ks_nlabels, _ = calculate_ks_distance("nlabels", X, y, k=k)
+
+    X0 = X[y == 0]
+    X1 = X[y == 1]
+    mmd_value = compute_mmd(X0, X1)
+
+    meta = [ks_distance, ks_density, ks_dimension, ks_nlabels, mmd_value]
+    return float(rf_model.predict([meta])[0])
